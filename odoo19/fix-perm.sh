716adc9cae150a59a3fa2698acae36d81ded3546
nimdasx <nimdasx@gmail.com>
2025-10-13 14:46:06 +0700

    odoo19

---
+++ odoo19/fix-perm.sh
@@ -0,0 +1,6 @@
+mkdir -p odoox/etc-odoo
+sudo chown -R 100:100 odoox/etc-odoo
+mkdir odoox/var-lib-odoo
+sudo chown -R 100:100 odoox/var-lib-odoo
+mkdir pgadminx-var-lib-pgadmin
+sudo chown -R 5050:5050 pgadminx-var-lib-pgadmin
